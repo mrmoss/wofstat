d8f098522ea705a6344e1568e5fe661597b6f4c3
Your Name <you@example.com>
2015-05-29 15:09:47 -0800

    BSD dev...

--- src/netstat_bsd.cpp
+++ src/netstat_bsd.cpp
@@ -1,40 +1,12 @@
 #include <iostream>
-#include <sys/types.h>
-#include <sys/param.h>
 #include <sys/sysctl.h>
 #include <sys/socket.h>
-#include <sys/socketvar.h>
-#include <sys/un.h>
-#include <netinet/in.h>
-#include <net/route.h>
-#include <netinet/in_systm.h>
-#include <netinet/ip.h>
-#include <netinet/in_pcb.h>
-#include <netinet/tcp_fsm.h>
-#include <arpa/inet.h>
-#include <bitstring.h>
-#include <ctype.h>
-#include <err.h>
-#include <errno.h>
-#include <netdb.h>
-#include <pwd.h>
-#include <stdio.h>
-#include <strings.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <netinet/tcp.h>
-#include <netinet/tcp_timer.h>
-#include <netinet/tcpip.h>
-#include <netinet/tcp_var.h>
 
 #define PF_LIST_INET	1
 
 #ifdef INET6
 	#define PF_LIST_INET6	2
 #endif
-
-void sysctl_sucker(int *name, u_int namelen, void **vp, size_t *szp);
-void get_sockets(const char *mib);
 
 int main()
 {

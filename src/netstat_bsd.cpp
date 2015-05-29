af14522c7c79985be770d8cc8a875a0b6d38c3fa
Your Name <you@example.com>
2015-05-29 15:09:00 -0800

    BSD dev...

--- src/netstat_bsd.cpp
+++ src/netstat_bsd.cpp
@@ -38,40 +38,11 @@ void get_sockets(const char *mib);
 
 int main()
 {
-	get_sockets("net.inet6.tcp6.pcblist");
-	get_sockets("net.inet6.udp6.pcblist");
+	int name=-;
+	size_t size=CTL_MAXNAME;
+	std::cout<<"sysctlnametomib  "<<sysctlnametomib("net.inet.tcp.pcblist",&name,&size)<<std::endl;
+
 	return 0;
-}
-
-void sysctl_sucker(int *name, u_int namelen, void **vp, size_t *szp)
-{
-	int rc;
-	void *v;
-	size_t sz;
-
-	/* printf("name %p, namelen %u\n", name, namelen); */
-
-	v = NULL;
-	sz = 0;
-	do {
-		rc = sysctl(&name[0], namelen, v, &sz, NULL, 0);
-		if (rc == -1 && errno != ENOMEM)
-			err(1, "sysctl");
-		if (rc == -1 && v != NULL) {
-			free(v);
-			v = NULL;
-		}
-		if (v == NULL) {
-			v = malloc(sz);
-			rc = -1;
-		}
-		if (v == NULL)
-			err(1, "malloc");
-	} while (rc == -1);
-
-	*vp = v;
-	*szp = sz;
-	/* printf("got %zu at %p\n", sz, v); */
 }
 
 void get_sockets(const char *mib)

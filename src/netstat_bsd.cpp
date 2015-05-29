555bcc75e6b1a4c29f906c46a36f03f309b90d7d
Your Name <you@example.com>
2015-05-29 14:44:34 -0800

    BSD dev...

--- src/netstat_bsd.cpp
+++ src/netstat_bsd.cpp
@@ -10,8 +10,64 @@
 
 int main()
 {
-	int mib[]={CTL_NET,PF_LIST_INET,0,0,0,0};
-	size_t len;
-	std::cout<<sysctl(mib,6,NULL,&len,NULL,0)<<"\t"<<len<<std::endl;
+	get_sockets("net.inet6.tcp6.pcblist");
+	get_sockets("net.inet6.udp6.pcblist");
 	return 0;
+}
+
+void sysctl_sucker(int *name, u_int namelen, void **vp, size_t *szp)
+{
+	int rc;
+	void *v;
+	size_t sz;
+
+	/* printf("name %p, namelen %u\n", name, namelen); */
+
+	v = NULL;
+	sz = 0;
+	do {
+		rc = prog_sysctl(&name[0], namelen, v, &sz, NULL, 0);
+		if (rc == -1 && errno != ENOMEM)
+			err(1, "sysctl");
+		if (rc == -1 && v != NULL) {
+			free(v);
+			v = NULL;
+		}
+		if (v == NULL) {
+			v = malloc(sz);
+			rc = -1;
+		}
+		if (v == NULL)
+			err(1, "malloc");
+	} while (rc == -1);
+
+	*vp = v;
+	*szp = sz;
+	/* printf("got %zu at %p\n", sz, v); */
+}
+
+void get_sockets(const char *mib)
+{
+	void *v;
+	size_t sz;
+	int rc, n, name[CTL_MAXNAME];
+	u_int namelen;
+
+	sz = CTL_MAXNAME;
+	rc = sysctlnametomib(mib, &name[0], &sz);
+	if (rc == -1) {
+		if (errno == ENOENT)
+			return;
+		err(1, "sysctlnametomib: %s", mib);
+	}
+	namelen = sz;
+
+	name[namelen++] = PCB_ALL;
+	name[namelen++] = 0;		/* XXX all pids */
+	name[namelen++] = sizeof(struct kinfo_pcb);
+	name[namelen++] = INT_MAX;	/* all of them */
+
+	sysctl_sucker(&name[0], namelen, &v, &sz);
+	n = sz / sizeof(struct kinfo_pcb);
+	socket_add_hash(v, n);
 }

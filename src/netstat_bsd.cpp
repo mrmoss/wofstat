f2940474836118c7d9caa33a86320ff20a7f7bd0
Your Name <you@example.com>
2015-05-29 14:27:37 -0800

    BSD dev...

--- src/netstat_bsd.cpp
+++ src/netstat_bsd.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include <sys/sysctl.h>
-#include <netinet/in.h>
-#include <net/if.h>
-#include <net/route.h>
+#include <sys/socket.h>
 
 int main()
 {
-	int mib[]={CTL_NET,PF_ROUTE,0,0,NET_RT_IFLIST2,0};
+	int mib[]={CTL_NET,PF_ROUTE,0,0,NET_RT_IFLIST,0};
 	size_t len;
 	std::cout<<sysctl(mib,6,NULL,&len,NULL,0)<<"\t"<<len<<std::endl;
 	return 0;

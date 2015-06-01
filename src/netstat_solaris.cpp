e762b88974209ef520b7b309ea875cdcd60bf401
Your Name <you@example.com>
2015-05-31 17:46:09 -0800

    ...

--- src/netstat_solaris.cpp
+++ src/netstat_solaris.cpp
@@ -143,27 +143,27 @@ typedef std::vector<netstat_t> netstat_list_t;
 
 void netstat_print(const netstat_t& netstat)
 {
-        std::cout<<
-                std::setw(4)<<netstat.proto<<" "<<
-                std::setw(64)<<netstat.local_address+":"+netstat.local_port<<" "<<
-                std::setw(64)<<netstat.foreign_address+":"+netstat.foreign_port<<" "<<
-                std::setw(16)<<netstat.state<<" "<<
-                std::setw(8)<<netstat.pid<<" "<<
-                std::endl;
+	std::cout<<
+		std::setw(4)<<netstat.proto<<" "<<
+		std::setw(64)<<netstat.local_address+":"+netstat.local_port<<" "<<
+		std::setw(64)<<netstat.foreign_address+":"+netstat.foreign_port<<" "<<
+		std::setw(16)<<netstat.state<<" "<<
+		std::setw(8)<<netstat.pid<<" "<<
+		std::endl;
 }
 
 void netstat_list_print(const netstat_list_t& netstats)
 {
-        std::cout<<
-                std::setw(4)<<"proto "<<
-                std::setw(64)<<"local_address "<<
-                std::setw(64)<<"foreign_address "<<
-                std::setw(16)<<"state "<<
-                std::setw(8)<<"pid "<<
-                std::endl;
+	std::cout<<
+		std::setw(4)<<"proto "<<
+		std::setw(64)<<"local_address "<<
+		std::setw(64)<<"foreign_address "<<
+		std::setw(16)<<"state "<<
+		std::setw(8)<<"pid "<<
+		std::endl;
 
-        for(size_t ii=0;ii<netstats.size();++ii)
-                netstat_print(netstats[ii]);
+	for(size_t ii=0;ii<netstats.size();++ii)
+			netstat_print(netstats[ii]);
 }
 
 int main()
@@ -342,14 +342,14 @@ int main()
 
 	netstat_list_t netstats;
 
-        for(size_t ii=0;ii<tcp4.size();++ii)
-                netstats.push_back(tcp4[ii]);
-        for(size_t ii=0;ii<tcp6.size();++ii)
-                netstats.push_back(tcp6[ii]);
-        for(size_t ii=0;ii<udp4.size();++ii)
-                netstats.push_back(udp4[ii]);
-        for(size_t ii=0;ii<udp6.size();++ii)
-                netstats.push_back(udp6[ii]);
+	for(size_t ii=0;ii<tcp4.size();++ii)
+			netstats.push_back(tcp4[ii]);
+	for(size_t ii=0;ii<tcp6.size();++ii)
+			netstats.push_back(tcp6[ii]);
+	for(size_t ii=0;ii<udp4.size();++ii)
+			netstats.push_back(udp4[ii]);
+	for(size_t ii=0;ii<udp6.size();++ii)
+			netstats.push_back(udp6[ii]);
 
 	netstat_list_print(netstats);
 

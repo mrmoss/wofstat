d8af9b70dda851a0ffd1299300b8b7c2c610de62
Mike Moss <mrmoss@alaska.edu>
2016-02-23 13:34:18 -0900

    .

--- src/generate.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef GENFIREWALL_HPP
-#define GENFIREWALL_HPP
-
-#include <string>
-#include "netstat.hpp"
-
-std::string generate_iptables(const netstat_list_t& netstats);
-
-#endif
--- src/genfw.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "generate.hpp"
-
-int main()
-{
-	std::cout<<generate_iptables(netstat())<<std::flush;
-	return 0;
-}

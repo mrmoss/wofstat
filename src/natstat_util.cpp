e5917d28c2f9a3e0e076f8078667c9ea83b79e53
Mike Moss <mrmoss@alaska.edu>
2016-03-10 13:01:33 -0900

    Auto sizing columns for human readable output.

--- src/natstat_util.cpp
+++ src/natstat_util.cpp
@@ -4,8 +4,17 @@
 #include <iostream>
 #include <sstream>
 
-void print_human(const natstat_t& natstat)
+void print_human(const natstat_t& natstat,std::vector<size_t> cols)
 {
+	if(cols.size()<4)
+	{
+		cols.clear();
+		cols.push_back(5);
+		cols.push_back(46);
+		cols.push_back(46);
+		cols.push_back(16);
+		cols.push_back(8);
+	}
 	bool V6=false;
 	if(natstat.proto.substr(3,1)=="6")
 		V6=true;
@@ -20,25 +29,45 @@ void print_human(const natstat_t& natstat)
 	else
 		rstr<<natstat.faddr<<":"<<natstat.fport;
 	std::cout<<
-		std::setw(5)<<natstat.proto<<
-		std::setw(46)<<lstr.str()<<
-		std::setw(46)<<rstr.str()<<
-		std::setw(16)<<natstat.state<<
-		std::setw(8)<<natstat.pid<<
+		std::setw(cols[0])<<natstat.proto<<
+		std::setw(cols[1])<<lstr.str()<<
+		std::setw(cols[2])<<rstr.str()<<
+		std::setw(cols[3])<<natstat.state<<
+		std::setw(cols[4])<<natstat.pid<<
 		std::endl;
 }
 
 void print_human(const natstat_list_t& natstats)
 {
+	size_t pad=1;
+	std::vector<size_t> cols;
+	cols.push_back(5+pad);
+	cols.push_back(15+pad);
+	cols.push_back(15+pad);
+	cols.push_back(5+pad);
+	cols.push_back(3+pad);
+	for(size_t ii=0;ii<natstats.size();++ii)
+	{
+		if(natstats[ii].proto.size()+pad>cols[0])
+			cols[0]=natstats[ii].proto.size()+pad;
+		if(natstats[ii].laddr.size()+7+pad>cols[1])
+			cols[1]=natstats[ii].laddr.size()+7+pad;
+		if(natstats[ii].faddr.size()+7>cols[2])
+			cols[2]=natstats[ii].faddr.size()+7+pad;
+		if(natstats[ii].state.size()+pad>cols[3])
+			cols[3]=natstats[ii].state.size()+pad;
+		if(natstats[ii].pid.size()+pad>cols[4])
+			cols[4]=natstats[ii].pid.size()+pad;
+	}
 	std::cout<<
-		std::setw(5)<<"proto"<<
-		std::setw(46)<<"local address"<<
-		std::setw(46)<<"foreign address"<<
-		std::setw(16)<<"state"<<
-		std::setw(8)<<"pid"<<
+		std::setw(cols[0])<<"proto"<<
+		std::setw(cols[1])<<"local address"<<
+		std::setw(cols[2])<<"foreign address"<<
+		std::setw(cols[3])<<"state"<<
+		std::setw(cols[4])<<"pid"<<
 		std::endl;
 	for(size_t ii=0;ii<natstats.size();++ii)
-			print_human(natstats[ii]);
+		print_human(natstats[ii],cols);
 }
 
 void print_wof(const natstat_t& natstat)

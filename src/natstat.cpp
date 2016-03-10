af59898235b106fde6794bcf62721cec9ca5fd4c
Mike Moss <mrmoss@alaska.edu>
2016-03-10 12:46:37 -0900

    Titles and such go to std error.

--- src/natstat.cpp
+++ src/natstat.cpp
@@ -5,31 +5,35 @@
 #include "string_util.hpp"
 #include <stdexcept>
 
-void show_help(const int return_code)
+void show_help()
 {
-	std::ostream* ostr=&std::cout;
-	if(return_code!=0)
-		ostr=&std::cerr;
-	*ostr<<"  Usage:  ./natstat [--help][h][--wof][-w]"<<std::endl;
-	*ostr<<"  --help, -h  Show help menu."<<std::endl;
-	*ostr<<"  --wof, -w   Print in wof format."<<std::endl;
-	exit(return_code);
+	std::cerr<<"  Usage:  ./natstat [--help][h][--wof][-w]"<<std::endl;
+	std::cerr<<"  --help, -h  Show help menu."<<std::endl;
+	std::cerr<<"  --wof, -w   Print in wof format."<<std::endl;
 }
 
 int main(int argc,char* argv[])
 {
 	try
 	{
+		std::cerr<<"Not Another Netstat"<<std::endl;
 		bool human_readable=true;
 		for(int ii=1;ii<argc;++ii)
 		{
 			std::string cli(argv[ii]);
 			if(cli=="--help"||cli=="-h")
-				show_help(0);
+			{
+				show_help();
+				return 1;
+			}
 			else if(cli=="--wof"||cli=="-w")
+			{
 				human_readable=false;
+			}
 			else
+			{
 				throw std::runtime_error("Unknown cli argument \""+cli+"\".");
+			}
 		}
 		natstat_list_t natstats(natstat());
 		if(human_readable)
@@ -40,12 +44,14 @@ int main(int argc,char* argv[])
 	catch(std::runtime_error& error)
 	{
 		std::cerr<<"Error - "<<error.what()<<std::endl;
-		show_help(1);
+		show_help();
+		exit(1);
 	}
 	catch(...)
 	{
 		std::cerr<<"Unknown error occured."<<std::endl;
-		show_help(1);
+		show_help();
+		exit(1);
 	}
 	return 0;
 }
